93b3d1df605f7a09e02c21b5e1e380142b02c519
ProgrammingGod <harry.richard.johnson@gmail.com>
2016-07-04 14:16:36 +1200

    int to double
    changed ints to double so can be used on larger numbers, however limited
    by stack

---
+++ FactorialRecursion.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by HarryJohnson on 30/06/2016.
+ */
+public class FactorialRecursion {
+
+    public static int findFactorial(int i, int result){
+        result *= i;
+        if(i > 1){
+            result = findFactorial((i - 1), result);
+        }else if (i == 0){
+            result = 1;
+        }
+            return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a positive integer to find the factorial of:");
+        int input = keyboard.nextInt();
+        int result = 1;
+        result = findFactorial(input, result);
+        System.out.println(result);
+    }
+}
